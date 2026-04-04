54237731bf5cbf5b595f61e7e0cfd695af036874
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-04-04 16:58:32 +0200

    add autotag

--- usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVGarden/__init__.py
@@ -14,6 +14,7 @@ import gettext
 
 PLUGIN_NAME = "TVGarden"
 PLUGIN_VERSION = "2.3"
+__version__ = PLUGIN_VERSION
 PLUGIN_PATH = resolveFilename(
     SCOPE_PLUGINS,
     "Extensions/{}".format(PLUGIN_NAME))
